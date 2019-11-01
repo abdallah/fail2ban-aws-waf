28cb5cbcaa722656bc35c325ab2facd2626d2e04
Abdallah Deeb <abdallah.d@onthegosystems.com>
2019-11-01 10:38:57 +0200

    switch to using boto3 instead of awscli

--- src/fail2ban_aws_waf.py
+++ src/fail2ban_aws_waf.py
@@ -1,94 +1,82 @@
 #!/usr/bin/env python
-
-import subprocess
-import json
 import argparse
+import boto3
+import os
+import json
 import logging
 import traceback
-import os
 from time import gmtime, strftime
 
 
-def exec_command(cmd):
-    log_message(logging.INFO, 'Executing command: {}'.format(cmd))
-
-    p = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
-
-    out = out.encode('ascii', 'ignore')
-    err = err.encode('ascii', 'ignore')
-
-    if err is not '':
-        raise RuntimeError(err)
-
-    return json.loads(out)
-
-
-def get_aws_waf_api_name():
-    return 'aws waf-regional' if AWS_GLOBAL is False else 'aws waf'
-
-
 def get_change_token():
-    result = exec_command('{} get-change-token'.format(get_aws_waf_api_name()))
-
-    if 'ChangeToken' not in result:
+    waf = boto3.client('waf-regional')
+    response = waf.get_change_token()
+    if 'ChangeToken' not in response:
         raise RuntimeError('Could not find ChangeToken in AWS API response')
-
-    return result['ChangeToken']
-
-
-def create_ip_updates_param(action, ip):
-    return json.dumps(
-        [{'Action': action.upper(), 'IPSetDescriptor': {'Type': 'IPV4', 'Value': '{}/32'.format(ip)}}],
-        separators=(',', ':')
-    )
+    else:
+        return response['ChangeToken']
 
 
-def is_ip_in_ip_sets(ip_set_id, ip):
-    result = exec_command('{} get-ip-set --ip-set-id {}'.format(get_aws_waf_api_name(), ip_set_id))
+# def get_ip_set_id():
+#     waf = boto3.client('waf-regional')
+#     response = waf.list_ip_sets()
+#     if response['IPSets']:
+#         return response['IPSets'][0].get('IPSetId')
+#     else:
+#         return []
 
-    if 'IPSet' not in result:
+
+def is_in_ip_sets(ip_set_id, ip_address):
+    waf = boto3.client('waf-regional')
+    response = waf.get_ip_set(IPSetId=ip_set_id)
+    if 'IPSet' not in response:
         raise RuntimeError('Could not find IPSet in AWS API response')
-
-    if 'IPSetDescriptors' not in result['IPSet']:
-        raise RuntimeError('Could not find IPSetDescriptors in AWS API response')
-
-    ips = result['IPSet']['IPSetDescriptors']
-
-    for ip_in_waf in ips:
-        if ip_in_waf['Value'] == '{}/32'.format(ip):
+    if 'IPSetDescriptors' not in response['IPSet']:
+        raise RuntimeError(
+            'Could not find IPSetDescriptors in AWS API response')
+    for ip in response['IPSet']['IPSetDescriptors']:
+        if ip['Value'] == '{}/32'.format(ip_address):
             return True
 
     return False
 
 
-def update_ip_set(change_token, ip_set_id, action, ip):
-    if action == 'INSERT' and is_ip_in_ip_sets(ip_set_id, ip):
-        log_message(logging.INFO, 'Attempt to ban IP {}. IP is already in IP set in WAF. Stopping script.'.format(ip))
-
+def update_ip_set(action, change_token, ip_set_id, ip_address):
+    if action == 'INSERT' and is_in_ip_sets(ip_set_id, ip_address):
+        log_message(
+            logging.INFO, 'Attempt to ban IP {}. IP is already in IP set in WAF. Stopping script.'.format(ip_address))
         return
-
-    if action == 'DELETE' and not is_ip_in_ip_sets(ip_set_id, ip):
-        log_message(logging.INFO, 'Attempt to unban IP {}. IP is not in IP set in WAF. Stopping script.'.format(ip))
-
+    if action == 'DELETE' and not is_in_ip_sets(ip_set_id, ip_address):
+        log_message(
+            logging.INFO, 'Attempt to unban IP {}. IP is not in IP set in WAF. Stopping script.'.format(ip_address))
         return
+    updates = [
+        {
+            'Action': action.upper(),
+            'IPSetDescriptor': {
+                'Type': 'IPV4',
+                'Value': '{}/32'.format(ip_address)
+            }
+        }
+    ]
+    change_token = get_change_token()
+    if change_token:
+        waf = boto3.client('waf-regional')
+        response = waf.update_ip_set(
+            IPSetId=ip_set_id,
+            ChangeToken=change_token,
+            Updates=updates
+        )
+        if 'ChangeToken' in response:
+            return response['ChangeToken']
 
-    result = exec_command("{} update-ip-set {} --change-token {} --ip-set-id {} --updates {}".format(
-        get_aws_waf_api_name(),
-        '--debug' if AWS_DEBUG is True else '',
-        change_token,
-        ip_set_id,
-        create_ip_updates_param(action, ip)
-    ))
-
-    if 'ChangeToken' not in result:
-        raise RuntimeError('Could not find ChangeToken in AWS API response')
-
-    return result['ChangeToken']
+    raise RuntimeError('Could not find ChangeToken in AWS API response')
+    return False
 
 
 def parse_cli_args():
-    parser = argparse.ArgumentParser(description='Fail2ban AWS WAF auto ban/unban IP script.')
+    parser = argparse.ArgumentParser(
+        description='Fail2ban AWS WAF auto ban/unban IP script.')
 
     parser.add_argument(
         '--ip-set-id',
@@ -159,7 +147,8 @@ if __name__ == '__main__':
     AWS_LOGGER = logging.getLogger('fail2ban_aws_waf')
     AWS_LOGGER.setLevel(logging.INFO)
 
-    formatter = logging.Formatter('%(asctime)s [{}] [%(levelname)s] %(message)s'.format(args.jail_name))
+    formatter = logging.Formatter(
+        '%(asctime)s [{}] [%(levelname)s] %(message)s'.format(args.jail_name))
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
     AWS_LOGGER.addHandler(stream_handler)
@@ -169,7 +158,8 @@ if __name__ == '__main__':
             AWS_LOGPATH = AWS_LOGPATH[0:-1]
 
         if not os.path.exists(AWS_LOGPATH):
-            raise RuntimeError('Log path {} does not exist.'.format(AWS_LOGPATH))
+            raise RuntimeError(
+                'Log path {} does not exist.'.format(AWS_LOGPATH))
 
         file_handler = logging.FileHandler('{}/{}'.format(
             AWS_LOGPATH,
@@ -181,14 +171,16 @@ if __name__ == '__main__':
 
     try:
         update_ip_set(
+            'INSERT' if args.action == 'ban' else 'DELETE',
             get_change_token(),
             args.ip_set_id,
-            'INSERT' if args.action == 'ban' else 'DELETE',
             args.ip
         )
     except Exception as e:
-        log_message(logging.ERROR, 'Exception cought: {}: {}'.format(type(e).__name__, str(e)))
-        log_message(logging.ERROR, 'Exception traceback: {}'.format(traceback.format_exc()))
+        log_message(logging.ERROR, 'Exception cought: {}: {}'.format(
+            type(e).__name__, str(e)))
+        log_message(logging.ERROR, 'Exception traceback: {}'.format(
+            traceback.format_exc()))
 
     AWS_LOGGER.removeHandler(stream_handler)
     stream_handler.close()
